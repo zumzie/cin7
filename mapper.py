4d4f8b8373565c469623a446e61a0980206adc7c
zumzie <73924544+zumzie@users.noreply.github.com>
2024-10-15 01:16:48 -0700

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -6,12 +6,31 @@ class Mapper:
 
 
     def mapProducts(self):
-        for i in self.productData:
-            pass
-        return self.productData
+        mapped_products = []
+
+        for product in self.productData:
+            new_prod = {}
+            new_prod['name'] = product['name']
+            new_prod['description'] = product['description']
+            new_prod['id'] = str(product['id'])
+            new_prod['product_identifier'] = product['styleCode']
+            mapped_products.append(new_prod)
+
+        return mapped_products
     
     def mapSkus(self):
-        pass
+        mapped_skus = []
+
+
+        #Iterate through product data then variants
+        for sku in self.productData:
+            new_sku = {}
+            new_sku['product_id'] = sku['product_id']
+            new_sku['external_id'] = sku['external_id']
+            new_sku['sku_identifier'] = sku['sku_identifier']
+            mapped_skus.append(new_sku)
+        
+        return mapped_skus
     
     def mapImages(self):
         pass
